220e4262d6f5821a59abdcbb973d3d05ff32259c
xuh30 <xuh30@chinatelecom.cn>
2023-12-24 00:00:31 +0800

    四则运算-基础运算

---
+++ basic_math/arithmetic_operations.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import re
+
+__author__ = "tom"
+
+
+class ArithmeticOperations:
+    def basic_calculate(self, expression: str):
+        try:
+            assert self.is_valid_calculation_expression(expression), Exception("Error: 表达式只能含有数字、小数点、括号和加减乘除符号。")
+            return eval(expression)
+        except Exception as error:
+            print(error.__str__())
+
+    def is_valid_calculation_expression(self, expression: str):
+        pattern = r"^[0-9().+\-*/]+$"
+        match = re.match(pattern, expression)
+        return match is not None
+
+
+if __name__ == "__main__":
+    print(ArithmeticOperations().basic_calculate("1+2"))
---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from basic_math.arithmetic_operations import ArithmeticOperations
+
+def print_hi(name):
+    # 在下面的代码行中使用断点来调试脚本。
+    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+
+
+# 按装订区域中的绿色按钮以运行脚本。
+if __name__ == '__main__':
+    print(ArithmeticOperations().basic_calculate("(1.2+a2)*3"))
+
