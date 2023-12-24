91b2a40ce90685877ee1dfc7e97e73a7c32ef134
Tom <xuhao2018@foxmail.com>
2023-12-24 13:34:38 +0800

    图形面积计算

--- basic_math/arithmetic_operations.py
+++ basic_math/arithmetic_operations.py
@@ -6,20 +6,17 @@ import re
 __author__ = "tom"
 
 
-class ArithmeticOperations:
-    def calculate(self, expression: str, reserve:int =2)->float:
-        try:
-            assert self.is_valid_calculation_expression(expression), Exception("Error: 表达式只能含有数字、小数点、括号和加减乘除符号。")
-            return round(eval(expression), reserve)
-        except Exception as error:
-            print(error.__str__())
+def is_valid_calculation_expression(expression: str):
+    pattern = r"^[0-9().+\-*/]+$"
+    match = re.match(pattern, expression)
+    return match is not None
 
 
-    def is_valid_calculation_expression(self, expression: str):
-        pattern = r"^[0-9().+\-*/]+$"
-        match = re.match(pattern, expression)
-        return match is not None
+def calculate(expression: str, reserve: int = 2) -> float:
+    try:
+        assert is_valid_calculation_expression(expression), Exception(
+            "Error: 表达式只能含有数字、小数点、括号和加减乘除符号。")
+        return round(eval(expression), reserve)
+    except Exception as error:
+        print(error.__str__())
 
-
-if __name__ == "__main__":
-    print(ArithmeticOperations().calculate("1+2"))
---
+++ geometry/area.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = "tom"
+
+pi = 3.141592653589793
+
+
+def rectangle_area(length: float, width: float, reserve: int = 2) -> float:
+    """
+    计算矩形面积
+    :param length: 矩形的长
+    :param width: 矩形的宽
+    :param reserve: 保留小数位数，默认为2
+    :return: 矩形面积
+    """
+    return round(length * width, reserve)
+
+
+def triangle_area(base: float, height: float, reserve: int = 2):
+    """
+    计算三角形面积
+    :param base: 三角形的底
+    :param height: 三角形的高
+    :param reserve: 保留小数位数，默认为2
+    :return: 三角形面积
+    """
+    return round(base * height * 0.5, reserve)
+
+
+def circle_area(radius: float, reserve: int = 2):
+    """
+    计算圆的面积
+    :param radius: 圆的半径
+    :param reserve: 保留小数位数，默认为2
+    :return: 圆的面积
+    """
+    return round(pi * radius**2, reserve)
+
+
+def parallelogram_area(base: float, height: float, reserve: int = 2):
+    """
+    计算平行四边形的面积
+    :param base: 平行四边形的底
+    :param height 平行四边形的高
+    :param reserve: 保留小数位数，默认为2
+    :return: 平行四边形面积
+    """
+    return round(base * height, reserve)
+
+
+def trapezium_area(base1: float, base2: float, height: float, reserve: int = 2):
+    """
+    计算梯形的面积
+    :param base1: 梯形的上底
+    :param base2: 梯形的下底
+    :param height 梯形的高
+    :param reserve: 保留小数位数，默认为2
+    :return: 梯形面积
+    """
+    return round((base1 * base2) * height / 2, reserve)
