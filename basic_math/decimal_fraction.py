a8297081f5da4cf870928fb0e31079a701764592
Tom <xuhao2018@foxmail.com>
2023-12-24 12:33:33 +0800

    小数和分数相互转化

---
+++ basic_math/decimal_fraction.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from fractions import Fraction
+
+__author__ = "tom"
+
+
+class DecimalFraction(object):
+
+    def decimal_to_fraction(num: float)->Fraction:
+        """
+        将小数转化为分数
+        """
+        return Fraction(num).limit_denominator()
+    
+    def fraction_to_decimal(expression: str=None, numerator=None, denominator=None, reserve:int =2)->float:
+        """
+        将分数转化为小数
+        """
+        if expression and (numerator is None and denominator is None):
+            assert '/' in expression, Exception("Error: 分数格式错误！")
+            numerator, denominator = tuple(map(int, expression.split("/")))
+        fraction = Fraction(numerator, denominator)
+        return round(float(fraction), reserve)
+
+
+
+if __name__=="__main__":
+    print(DecimalFraction.decimal_to_fraction(0.25))
+    print(DecimalFraction.fraction_to_decimal('1/4'))
