46b08777bc1e57045c489660418bdd0256176f4e
Tom <xuhao2018@foxmail.com>
2023-12-25 17:07:49 +0800

    计算长方体和正方体的面积

--- geometry/area.py
+++ geometry/area.py
@@ -17,7 +17,7 @@ def rectangle_area(length: float, width: float, reserve: int = 2) -> float:
     return round(length * width, reserve)
 
 
-def triangle_area(base: float, height: float, reserve: int = 2):
+def triangle_area(base: float, height: float, reserve: int = 2) -> float:
     """
     计算三角形面积
     :param base: 三角形的底
@@ -28,7 +28,7 @@ def triangle_area(base: float, height: float, reserve: int = 2):
     return round(base * height * 0.5, reserve)
 
 
-def circle_area(radius: float, reserve: int = 2):
+def circle_area(radius: float, reserve: int = 2) -> float:
     """
     计算圆的面积
     :param radius: 圆的半径
@@ -38,7 +38,7 @@ def circle_area(radius: float, reserve: int = 2):
     return round(pi * radius**2, reserve)
 
 
-def parallelogram_area(base: float, height: float, reserve: int = 2):
+def parallelogram_area(base: float, height: float, reserve: int = 2) -> float:
     """
     计算平行四边形的面积
     :param base: 平行四边形的底
@@ -49,13 +49,28 @@ def parallelogram_area(base: float, height: float, reserve: int = 2):
     return round(base * height, reserve)
 
 
-def trapezium_area(base1: float, base2: float, height: float, reserve: int = 2):
+def trapezium_area(
+    base1: float, base2: float, height: float, reserve: int = 2
+) -> float:
     """
     计算梯形的面积
     :param base1: 梯形的上底
     :param base2: 梯形的下底
-    :param height 梯形的高
+    :param height: 梯形的高
     :param reserve: 保留小数位数，默认为2
     :return: 梯形面积
     """
-    return round((base1 * base2) * height / 2, reserve)
+    return round((base1 + base2) * height / 2, reserve)
+
+
+def cuboid_area(length: float, width: float, height: float, reserve: int = 2) -> float:
+    """
+    计算长方体和正方体的面积
+    :param length: 长方体和正方体的长
+    :param width: 长方体和正方体的宽
+    :param height: 长方体和正方体的高
+    :param reserve: 保留小数位数，默认为2
+    :return: 长方体和正方体面积
+    """
+    area = (length * width + length * height + width * height) * 2
+    return round(area, reserve)
