bd17c4f64722597bc58443c19a0fc975ae6e0c3f
Tom <xuhao2018@foxmail.com>
2023-12-27 14:28:12 +0800

    序列的中位数、极差、变异系数、集中趋势和离散程度

--- statistic/descriptive_statistics.py
+++ statistic/descriptive_statistics.py
@@ -71,3 +71,62 @@ def sequence_mode(nums: list) -> list:
     return res
 
 
+def sequence_median(nums: list) -> object:
+    """
+    计算序列的中位数
+    :param nums: 序列
+    :return: 序列中位数
+    """
+    sort_nums = sorted(nums)
+    mid = len(sort_nums) // 2
+    if len(sort_nums) % 2 == 0:
+        return (sort_nums[mid - 1] + sort_nums[mid]) / 2
+    else:
+        return sort_nums[mid]
+
+
+def sequence_range(nums: list, reserve: int = 2) -> float:
+    """
+    获取序列的极差
+    :param nums: 序列
+    :param reserve: 保留小数位数，默认为2
+    :return: 序列的极差
+    """
+    return round(max(nums) - min(nums), reserve)
+
+
+def sequence_variable_coefficient(nums: list, reserve: int = 2) -> float:
+    """
+    计算序列的变异系数
+    :param nums: 序列
+    :param reserve: 保留小数位数，默认为2
+    :return: 序列的变异系数
+    """
+    return round(sequence_std(nums, reserve=reserve + 1) / sequence_mean(nums, reserve=reserve + 1), reserve)
+
+
+def sequence_central_tendency(nums: list, reserve: int = 2) -> str:
+    """
+    获取序列的集中趋势（平均数、众数、中位数）
+    :param nums: 序列
+    :param reserve: 保留小数位数，默认为2
+    :return: 序列的集中趋势（平均数、众数、中位数）
+    """
+    return (f"集中趋势：\n"
+            f"平均数:\t{sequence_mean(nums, reserve)}\n"
+            f"众数:\t{sequence_mode(nums)}\n"
+            f"中位数:\t{sequence_median(nums)}")
+
+
+def sequence_dispersion_degree(nums: list, reserve: int = 2) -> str:
+    """
+    获取序列的离散程度（极差、方差、标准差、变异系数）
+    :param nums:
+    :param reserve:
+    :return:
+    """
+    return (f"离散程度：\n"
+            f"极差:\t{sequence_range(nums, reserve)}\n"
+            f"方差:\t{sequence_variance(nums, reserve)}\n"
+            f"标准差:\t{sequence_std(nums, reserve)}\n"
+            f"变异系数:\t{sequence_variable_coefficient(nums, reserve)}")
