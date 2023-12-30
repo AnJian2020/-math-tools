5f3987a06959221a3194a857eacf34023b032d92
Tom <xuhao2018@foxmail.com>
2023-12-30 23:53:54 +0800

    基础算法

--- basic_math/arithmetic_operations.py
+++ basic_math/arithmetic_operations.py
@@ -6,13 +6,24 @@ import re
 __author__ = "tom"
 
 
-def is_valid_calculation_expression(expression: str):
+def is_valid_calculation_expression(expression: str)->bool:
+    """
+    判断计算的表达式是否只含有数字、小数点、括号和加减乘除符号
+    :param expression: 表达式
+    :return: 判断结果
+    """
     pattern = r"^[0-9().+\-*/]+$"
     match = re.match(pattern, expression)
     return match is not None
 
 
 def calculate(expression: str, reserve: int = 2) -> float:
+    """
+    计算表达式
+    :param expression: 表达式
+    :param reserver: 保留小数位数，默认为2
+    :return: 判断结果
+    """
     assert is_valid_calculation_expression(expression), Exception(
         "Error: 表达式只能含有数字、小数点、括号和加减乘除符号。"
     )
--- basic_math/decimal_fraction.py
+++ basic_math/decimal_fraction.py
@@ -9,13 +9,20 @@ __author__ = "tom"
 def decimal_to_fraction(num: float) -> Fraction:
     """
     将小数转化为分数
+    :param nums: 序列
+    :return: 分数
     """
     return Fraction(num).limit_denominator()
 
 
 def fraction_to_decimal(expression: str = None, numerator=None, denominator=None, reserve: int = 2) -> float:
     """
-    将分数转化为小数
+    将分数转化为小数，分数表达式与分母、分子不能同时为空
+    :param expression: 分数表达式
+    :param numerator: 分子
+    :param denominator: 分母
+    :param reserver: 保留小数位数，默认为2
+    :return: 小数
     """
     if expression:
         assert '/' in expression, Exception("Error: 分数格式错误！")
